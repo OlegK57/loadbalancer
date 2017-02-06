658d2257662266f39eeb2f05206d65aeb57c3b60
Oleg Kasyanov <devel@ogk.in.ua>
2017-02-06 02:48:27 +0200

    Add subnet check

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,8 @@ MAINTAINER OlegK57
 LABEL maintainer "OlegK57@gmail.com"
 
 COPY haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
+COPY my_run.sh /my_run.sh
+
+RUN chmod a+x /my_run.sh
+
+CMD /my_run.sh
---
+++ my_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SUBNET=`ip -4 addr show|grep global|sed 's/[ \t]*inet[ \t]*//'|cut -d. -f1-3`
+
+sed -i -e "s/ADDR/$SUBNET/g" /usr/local/etc/haproxy/haproxy.cfg
+
+/docker-entrypoint.sh
