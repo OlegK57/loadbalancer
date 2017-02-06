23c2bc939c9616d693fcdf39070dc04a916c0d60
Oleg Kasyanov <devel@ogk.in.ua>
2017-02-06 03:32:35 +0200

    Little fix

--- my_run.sh
+++ my_run.sh
@@ -4,4 +4,4 @@ SUBNET=`ip -4 addr show|grep global|sed 's/[ \t]*inet[ \t]*//'|cut -d. -f1-3`
 
 sed -i -e "s/ADDR/$SUBNET/g" /usr/local/etc/haproxy/haproxy.cfg
 
-/docker-entrypoint.sh
+./docker-entrypoint.sh
