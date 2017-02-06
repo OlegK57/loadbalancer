dce9a83a383a9ffbac6b7470a9c83888637bb769
Oleg Kasyanov <devel@ogk.in.ua>
2017-02-06 04:00:53 +0200

    Change run script

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ SUBNET=`ip -4 addr show|grep global|sed 's/[ \t]*inet[ \t]*//'|cut -d. -f1-3`
 
 sed -i -e "s/ADDR/$SUBNET/g" /usr/local/etc/haproxy/haproxy.cfg
 
-bash /docker-entrypoint.sh
+bash /docker-entrypoint.sh haproxy -f /usr/local/etc/haproxy/haproxy.cfg
