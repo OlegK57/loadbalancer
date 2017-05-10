51b6b3bddc68dab450e3000f5a024d95e4c5489c
Oleg Kasyanov <devel@ogk.in.ua>
2017-05-10 20:00:49 +0300

    Delete unused files

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SUBNET=`ip -4 addr show|grep global|sed 's/[ \t]*inet[ \t]*//'|cut -d. -f1-3`
-
-sed -i -e "s/ADDR/$SUBNET/g" /usr/local/etc/haproxy/haproxy.cfg
-
-bash /docker-entrypoint.sh haproxy -f /usr/local/etc/haproxy/haproxy.cfg
